3643b0817f12c690d0edfd90795b54a85d79f586
Hiển <hoilamgi7999@gmail.com>
2019-01-20 14:31:22 +0700

    Updates

---
+++ precache-manifest.580db463c1b56215cca39d1912e19539.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f7ec7375100b843b2c554fa31d0e531",
+    "url": "/boredapp/static/media/mobile-bg.2f7ec737.svg"
+  },
+  {
+    "revision": "6dd553d129805e548795",
+    "url": "/boredapp/static/js/runtime~main.6dd553d1.js"
+  },
+  {
+    "revision": "2a5a90eb8efb8e99876e",
+    "url": "/boredapp/static/js/main.2a5a90eb.chunk.js"
+  },
+  {
+    "revision": "727c36f6aa0d736be332",
+    "url": "/boredapp/static/js/1.727c36f6.chunk.js"
+  },
+  {
+    "revision": "727c36f6aa0d736be332",
+    "url": "/boredapp/static/css/1.a54f8f7f.chunk.css"
+  },
+  {
+    "revision": "3ebb2ac90144cf05c4746c9d27a035ed",
+    "url": "/boredapp/index.html"
+  }
+];
